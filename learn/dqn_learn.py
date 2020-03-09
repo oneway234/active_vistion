2698dadadc01469f8a1d9d05d7b2b1ccfa54655c
wei <wangweichuse@gmail.com>
2020-03-09 11:22:37 +0800

    update main.py > action choose inimg update DQN fc4 7*7*67 > 1978624

--- learn/dqn_learn.py
+++ learn/dqn_learn.py
@@ -24,14 +24,19 @@ class DQN(object):
         self.loss_func = nn.MSELoss()
 
     def choose_action(self, x):
-        x = torch.unsqueeze(torch.FloatTensor(x), 0) #若輸入為圖片？不知道怎麼改，顯示只接受str
+        # x = torch.unsqueeze(torch.FloatTensor(x), 0)
         # input only one sample
         if np.random.uniform() < EPSILON:   # greedy
-            actions_value = self.eval_net.forward(x)
+            actions_value, _ = self.eval_net.forward(x)
             action = torch.max(actions_value, 1)[1].data.numpy()
         else:   # random
             action = np.random.randint(0, N_ACTIONS-1)
         return action
+
+    def img_feature(self, x):
+        x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        _, feature = self.eval_net.forward(x)
+        return feature
 
     def store_transition(self, s, a, r, s_):
         transition = np.hstack((s, [a, r], s_))
--- main.py
+++ main.py
@@ -3,30 +3,39 @@ from learn.dqn_learn import DQN
 import os
 import cv2
 import numpy as np
+import torch
 
 MEMORY_CAPACITY = 2000
 def run_acd():
     dqn = DQN()
     steps = 0
-    for episode in range(1000):
+    for episode in range(10):
         # initial observation
         print("initial observation")
         train_set, img, thing_label, diff = env.reset(episode) # observation:
         while True:
             # RL choose action based on observation
-            inimg = os.path.join(train_set,'jpg_rgb', img)
-            # inimg = Image.open(inimg).convert('RGB')
-            inimg = cv2.imread(inimg)
-            inimg = np.transpose(inimg, (2, 0, 1))
-            action = dqn.choose_action(inimg)
+            inimg = os.path.join(train_set, 'jpg_rgb', img)#read curr img
+            inimg = read_img(inimg)
+            print("choose action...")
+            action = dqn.choose_action(inimg) #choose action
+            curr_s = dqn.eval_net.forward(inimg) #input img into net
+
 
             # RL take action and get next observation and reward
+            print("next stste")
             reward, next_img, next_diff = env.step(train_set, img, thing_label, diff, action)
 
-            s = img
-            s_ = next_img
+            inextimg = os.path.join(train_set, 'jpg_rgb', next_img)  # read next img
+            inextimg = read_img(inextimg)
+            next_s = dqn.eval_net.forward(inextimg)
+
+            s = curr_s
+            s_ = next_s
             r = reward
             a = action
+            print("store")
+            print("s", type(s), "\na:", a, "\nr:", r, "\ns:", s_)
             dqn.store_transition(s, a, r, s_)
 
             if dqn.memory_counter > MEMORY_CAPACITY:
@@ -37,6 +46,13 @@ def run_acd():
             steps += 1
             img = next_img
 
+    torch.save(dqn, 'dqn.pkl')
+
+def read_img(img):
+    inimg = cv2.imread(img)
+    inimg = np.transpose(inimg, (2, 0, 1))
+    inimg = torch.unsqueeze(torch.FloatTensor(inimg), 0)
+    return inimg
 
 def stopping_criterion(next_diff, steps):
     if next_diff == 1 or steps >= 100:
