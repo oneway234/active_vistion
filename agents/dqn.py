7d3ef5d8b85e3b40b8019b5562cade2528071fe4
wei <wangweichuse@gmail.com>
2020-03-29 18:54:00 +0800

    only left fc layer

--- agents/dqn.py
+++ agents/dqn.py
@@ -12,15 +12,9 @@ class DQN(nn.Module):
             num_actions: number of action-value to output, one-to-one correspondence to action in game.
         """
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 512, kernel_size=(1, 8), stride=4)
-        self.conv2 = nn.Conv2d(512, 64, kernel_size=(1, 4), stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=(1, 3), stride=1)
-        self.fc4 = nn.Linear(3840, 512)
-        self.fc5 = nn.Linear(512, num_actions)
+        self.fc1 = nn.Linear(3840, 512)
+        self.fc2 = nn.Linear(512, num_actions)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.fc4(x.view(x.size(0), -1)))
-        return self.fc5(x)
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
+        return self.fc2(x)
